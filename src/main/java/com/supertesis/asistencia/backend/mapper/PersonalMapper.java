b9f690bea5755d7a7ccdc5ce2581d5bc33996b80
Douglas Ojeda <douglasojeda43@gmail.com>
2026-05-16 10:41:11 -0400

    ASIS-16 fix: Cambios en DTO y Mapper de Personal.
    - Se manejan dos DTO para personal para manejar de forma correcta la fecha de egreso y el estado activo.
    - En CreateRequest, se ignora el estado activo (por defecto es true en el Service) y fecha egreso (no es necesario para nuevos registros).
    - Se creó un nuevo método en Mapper para actualizar los datos de la entidad existente desde el UpdateRequest.
    - Se modificó el nombre del método "toEntity" a "toEntityCreate".

--- src/main/java/com/supertesis/asistencia/backend/mapper/PersonalMapper.java
+++ src/main/java/com/supertesis/asistencia/backend/mapper/PersonalMapper.java
@@ -1,12 +1,16 @@
 package com.supertesis.asistencia.backend.mapper;
 
-import com.supertesis.asistencia.backend.dto.personal.PersonalRequestDto;
+import com.supertesis.asistencia.backend.dto.personal.PersonalCreateRequestDto;
 import com.supertesis.asistencia.backend.dto.personal.PersonalResponseDto;
+import com.supertesis.asistencia.backend.dto.personal.PersonalUpdateRequestDto;
 import com.supertesis.asistencia.backend.entity.Personal;
 
+import org.mapstruct.BeanMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
 public interface PersonalMapper {
@@ -22,5 +26,16 @@ public interface PersonalMapper {
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "cargo", ignore = true)        // Lo manejará el Service
     @Mapping(target = "departamento", ignore = true) // Lo manejará el Service
-    Personal toEntity(PersonalRequestDto dto);
+    @Mapping(target = "fechaEgreso", ignore = true) // No se establece al crear
+    @Mapping(target = "activo", ignore = true)     // Se establece automáticamente al crear
+    Personal toEntityCreate(PersonalCreateRequestDto dto);
+
+    // De Petición a Entidad (Para actualizar)
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "cargo", ignore = true)        // Lo manejará el Service
+    @Mapping(target = "departamento", ignore = true) // Lo manejará el Service
+    void updateEntityFromDto(PersonalUpdateRequestDto dto, @MappingTarget Personal entity);
+
+
 }
